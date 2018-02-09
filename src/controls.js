86ede0dd21022e4f4bf875abc753d69b67ed265d
Ajit Bohra <ajit@lubus.in>
2018-02-09 22:05:17 +0530

    Change class components to functional components

--- src/controls.js
+++ src/controls.js
@@ -22,81 +22,77 @@ const {
 /**
 * Create an Inspector Controls wrapper Component
 */
-export default class Controls extends Component {
-    constructor(props) {
-        super(...arguments);
-    }
-
-    render() {
-        return (
-            <BlockControls key="toolbar">
-                <Toolbar>
-                    <IconButton
-                        icon={blockIcons.bbutton}
-                        label={__('Big Button')}
-                        onClick={() => this.props.onChangeTheme('bbutton')}
-                        className={
-                            classnames(
-                                { 'tld-selected-icon': (this.props.attributes.theme === 'bbutton') },
-                            )
-                        }
-                    />
-                    <IconButton
-                        icon={blockIcons.dashed}
-                        label={__('Dashed')}
-                        onClick={() => this.props.onChangeTheme('dashed')}
-                        className={
-                            classnames(
-                                { 'tld-selected-icon': (this.props.attributes.theme === 'dashed') },
-                            )
-                        }
-                    />
-                    <IconButton
-                        icon={blockIcons.minimalist}
-                        label={__('Minimalist')}
-                        onClick={() => this.props.onChangeTheme('minimalist')}
-                        className={
-                            classnames(
-                                { 'tld-selected-icon': (this.props.attributes.theme === 'minimalist') },
-                            )
-                        }
-                    />
-                </Toolbar>
-                <Toolbar>
-                    <DropdownMenu
-                        icon="editor-textcolor"
-                        label={__('Font')}
-                        menuLabel={__('Font')}
-                        controls={[
-                            {
-                                title: __('Poiret One'),
-                                icon: 'editor-textcolor',
-                                onClick: () => this.props.onChangeFont('poiret-one'),
-                            },
-                            {
-                                title: __('Lobster Two'),
-                                icon: 'editor-textcolor',
-                                onClick: () => this.props.onChangeFont('lobster-two'),
-                            },
-                            {
-                                title: __('Raleway'),
-                                icon: 'editor-textcolor',
-                                onClick: () => this.props.onChangeFont('raleway'),
-                            },
-                            {
-                                title: __('Titillium Web'),
-                                icon: 'editor-textcolor',
-                                onClick: () => this.props.onChangeFont('titillium-web'),
-                            },
-                            {
-                                title: __('Indie Flower'),
-                                icon: 'editor-textcolor',
-                                onClick: () => this.props.onChangeFont('indie-flower'),
-                            },
-                        ]}
-                    />
-                </Toolbar>
-            </BlockControls>
-        )
-    }
+const Controls = (props) => {
+    return (
+        <BlockControls key="toolbar">
+            <Toolbar>
+                <IconButton
+                    icon={blockIcons.bbutton}
+                    label={__('Big Button')}
+                    onClick={() => props.onChangeTheme('bbutton')}
+                    className={
+                        classnames(
+                            { 'tld-selected-icon': (props.attributes.theme === 'bbutton') },
+                        )
+                    }
+                />
+                <IconButton
+                    icon={blockIcons.dashed}
+                    label={__('Dashed')}
+                    onClick={() => props.onChangeTheme('dashed')}
+                    className={
+                        classnames(
+                            { 'tld-selected-icon': (props.attributes.theme === 'dashed') },
+                        )
+                    }
+                />
+                <IconButton
+                    icon={blockIcons.minimalist}
+                    label={__('Minimalist')}
+                    onClick={() => props.onChangeTheme('minimalist')}
+                    className={
+                        classnames(
+                            { 'tld-selected-icon': (props.attributes.theme === 'minimalist') },
+                        )
+                    }
+                />
+            </Toolbar>
+            <Toolbar>
+                <DropdownMenu
+                    icon="editor-textcolor"
+                    label={__('Font')}
+                    menuLabel={__('Font')}
+                    controls={[
+                        {
+                            title: __('Poiret One'),
+                            icon: 'editor-textcolor',
+                            onClick: () => props.onChangeFont('poiret-one'),
+                        },
+                        {
+                            title: __('Lobster Two'),
+                            icon: 'editor-textcolor',
+                            onClick: () => props.onChangeFont('lobster-two'),
+                        },
+                        {
+                            title: __('Raleway'),
+                            icon: 'editor-textcolor',
+                            onClick: () => props.onChangeFont('raleway'),
+                        },
+                        {
+                            title: __('Titillium Web'),
+                            icon: 'editor-textcolor',
+                            onClick: () => props.onChangeFont('titillium-web'),
+                        },
+                        {
+                            title: __('Indie Flower'),
+                            icon: 'editor-textcolor',
+                            onClick: () => props.onChangeFont('indie-flower'),
+                        },
+                    ]}
+                />
+            </Toolbar>
+        </BlockControls>
+    )
 }
+
+export default Controls;
--- src/inspector.js
+++ src/inspector.js
@@ -2,7 +2,6 @@
  * Internal block libraries
  */
 const { __ } = wp.i18n;
-const { Component } = wp.element;
 const {
     InspectorControls,
     BlockControls,
@@ -21,81 +20,78 @@ const {
 /**
 * Create an Inspector Controls wrapper Component
 */
-export default class Inspector extends Component {
-    constructor(props) {
-        super(...arguments);
-    }
 
-    render() {
-        return (
-            <InspectorControls key="inspector">
-                <PanelBody title={__('Tweet Settings')} >
-                    <TextareaControl
-                        label={__('Tweet Text')}
-                        value={this.props.attributes.tweet}
-                        onChange={this.props.onChangeTweet}
-                        help={__('You can add hashtags and mentions here that will be part of the actual tweet, but not of the display on your post.')}
-                    />
-                    <TextControl
-                        label={__('Button Text')}
-                        value={this.props.attributes.button}
-                        onChange={this.props.onChangeButton}
-                    />
-                </PanelBody>
-                <PanelBody title={__('Animation Settings')} >
-                    <SelectControl
-                        label={__('Animation')}
-                        value={this.props.attributes.animation}
-                        options={[
-                            {
-                                label: __('None'),
-                                value: 'none',
-                            },
-                            {
-                                label: __('Pulse'),
-                                value: 'pulse',
-                            },
-                            {
-                                label: __('Tada'),
-                                value: 'tada',
-                            },
-                            {
-                                label: __('Bounce'),
-                                value: 'bounce',
-                            },
-                        ]}
-                        onChange={this.props.onChangeAnimation}
-                    />
-                    {this.props.attributes.animation !== 'none' ?
-                        <div className="if-animation-enable">
-                            <RangeControl
-                                label={__('Animation Duration (Seconds)')}
-                                value={this.props.attributes.duration || 1}
-                                onChange={this.props.onChangeDuration}
-                                min={1}
-                                max={9}
-                                beforeIcon="clock"
-                                allowReset
-                            />
-                            <RangeControl
-                                label={__('Animation Delay (Seconds)')}
-                                value={this.props.attributes.delay || 1}
-                                onChange={this.props.onChangeDelay}
-                                min={1}
-                                max={9}
-                                beforeIcon="clock"
-                                allowReset
-                            />
-                            <ToggleControl
-                                label={__('Loop Animation?')}
-                                checked={!!this.props.attributes.infinite}
-                                onChange={this.props.toggleInfinite}
-                            />
-                        </div> :
-                        null
-                    }
-                </PanelBody>
-            </InspectorControls>
-        )   
-    }
+const Inspector = (props) => {
+    return (
+        <InspectorControls key="inspector">
+            <PanelBody title={__('Tweet Settings')} >
+                <TextareaControl
+                    label={__('Tweet Text')}
+                    value={props.attributes.tweet}
+                    onChange={props.onChangeTweet}
+                    help={__('You can add hashtags and mentions here that will be part of the actual tweet, but not of the display on your post.')}
+                />
+                <TextControl
+                    label={__('Button Text')}
+                    value={props.attributes.button}
+                    onChange={props.onChangeButton}
+                />
+            </PanelBody>
+            <PanelBody title={__('Animation Settings')} >
+                <SelectControl
+                    label={__('Animation')}
+                    value={props.attributes.animation}
+                    options={[
+                        {
+                            label: __('None'),
+                            value: 'none',
+                        },
+                        {
+                            label: __('Pulse'),
+                            value: 'pulse',
+                        },
+                        {
+                            label: __('Tada'),
+                            value: 'tada',
+                        },
+                        {
+                            label: __('Bounce'),
+                            value: 'bounce',
+                        },
+                    ]}
+                    onChange={props.onChangeAnimation}
+                />
+                {props.attributes.animation !== 'none' ?
+                    <div className="if-animation-enable">
+                        <RangeControl
+                            label={__('Animation Duration (Seconds)')}
+                            value={props.attributes.duration || 1}
+                            onChange={props.onChangeDuration}
+                            min={1}
+                            max={9}
+                            beforeIcon="clock"
+                            allowReset
+                        />
+                        <RangeControl
+                            label={__('Animation Delay (Seconds)')}
+                            value={props.attributes.delay || 1}
+                            onChange={props.onChangeDelay}
+                            min={1}
+                            max={9}
+                            beforeIcon="clock"
+                            allowReset
+                        />
+                        <ToggleControl
+                            label={__('Loop Animation?')}
+                            checked={!!props.attributes.infinite}
+                            onChange={props.toggleInfinite}
+                        />
+                    </div> :
+                    null
+                }
+            </PanelBody>
+        </InspectorControls>
+    )   
 }
+
+export default Inspector;
